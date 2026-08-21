147b3a91f2f4f5a2740a46a4ae9ad1d9d7c1b5a1
KhanjanCS <khanjancs2024@gmail.com>
2026-08-21 13:54:03 +0530

    Q9 shapes done using interface

--- java_assignments/3/9.java
+++ java_assignments/3/9.java
@@ -8,18 +8,24 @@ Store multiple shapes in an array and calculate areas/perimeters using a single 
 interface Shape{
 	public double calculateArea();
 	public double calculatePerimeter();
+	public String getName();
 }
 
 class ShapeAreaPerimeter{
 	public static void main(String args[]){
 		Triangle t1 = new Triangle();
-		Circle c1 = new Circle(12);
+		Circle c1 = new Circle();
+		Rectangle r1 = new Rectangle();
 		
-		System.out.println(t1.calculateArea());
-		System.out.println(t1.calculatePerimeter());
+		Shape shps[] = {t1,c1,r1};
 		
-		System.out.println(c1.calculateArea());
-		System.out.println(c1.calculatePerimeter());
+		for (int i=0 ; i<shps.length ; i++){
+		System.out.println(shps[i].calculateArea());
+		System.out.println(shps[i].calculatePerimeter());
+		System.out.println(shps[i].getName());
+		
+		}
+		
 		
 	}
 	
@@ -41,6 +47,10 @@ class Triangle implements Shape{
 		s1 = 12;
 		s2 = 14;
 		s3 = 15;
+	}
+	
+	public String getName(){
+		return "Triangle";
 	}
 	
 	
@@ -81,8 +91,46 @@ class Circle implements Shape{
 	
 	public double calculatePerimeter(){
 		double peri = 2*pi*rad;
+		return peri;
+	}
+	
+	public String getName(){
+		return "Circle";
+	}
+		
+}
+
+class Rectangle implements Shape{
+	double l;
+	double b;
+	
+	Rectangle(double x,double y){
+		l = x;
+		b = y;
+	}
+	
+	Rectangle(){
+		l = 4;
+		b = 3;
+	}
+	
+	
+	
+	public double calculateArea(){
+		
+		double area = l*b;
+		
+		return area;
+	}
+	
+	public double calculatePerimeter(){
+		double peri = 2*(l+b);
 		
 		return peri;
+	}
+	
+	public String getName(){
+		return "Rectangle";
 	}
 		
 }
